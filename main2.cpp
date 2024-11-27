6d9969fc512a90948b88d6a05c8e0a5cb6dd3baf
ROKODIL1488 <akkdlaakka@gmail.com>
2024-11-27 15:32:37 +0300

    123

---
+++ main2.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+// int a;
+// cout << "размер";
+// cin >> a;
+// const int arr[a];
+// cout << "числа";
+// for (int i =0; i < a; ++i) {
+//     cin >> arr[i];
+// }
+
+// int b = arr[a-1];
+
+// for (int i = a-1; i >= 0; --i) {
+//     if (arr[i] > b) {
+//         b = arr[i];
+//     }
+// }
+// cout << "самое большое число " << b;
+// }
