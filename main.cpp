39e0fefbd66683a8bfeec938d829542b6d02ab5d
ROKODIL1488 <akkdlaakka@gmail.com>
2024-11-27 15:20:38 +0300

    добавил файл main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+int n, p = 0;
+cin >> n;
+vector<int> a;
+    for(int i = 0; i < n; i++){
+    int temp;
+    cin >> temp;
+    a.push_beck(temp);
+}
+    for(int i = 0;i < n; i ++){
+        if(a[i] > 0){
+            p++;}
+        cout << p;
+    }
+  return 0;
+}
